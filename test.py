7b1f67c600eb6bfd569be7553d1026ecc8df4443
Itay <95435881+Itayb3@users.noreply.github.com>
2022-04-03 20:00:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import os
 import time
 import shutil
 
+
 class Login:
     def __init__(self, root):
         self.root = root
@@ -30,14 +31,12 @@ class Login:
             Button(frame_login, cursor="hand2", text="IDF", bd=0, command=to_idf,
                    font=("Goudy old style", 15), bg="#6162FF", fg="white").place(x=255, y=150, width=180,
                                                                                  height=40)
-            Button(frame_login, cursor="hand2", text="Both", bd=0, command=to_both,
-                   font=("Goudy old style", 15), bg="#6162FF", fg="white").place(x=255, y=200, width=180,
+            Button(frame_login, cursor="hand2", text="Even & IDF", bd=0, command=to_both,
+                   font=("Goudy old style", 15), bg="#6162FF", fg="white").place(x=150, y=200, width=180,
                                                                                  height=40)
 
         def both_t_page():
-            Register = Frame(root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
-
+            Frame(root, bg="white", ).place(x=330, y=150, width=500, height=400)
 
             def prog():
                 def start():
@@ -52,14 +51,11 @@ class Login:
                         text.set(str(download) + "/" + str(GB) + " Uploading files")
                         root.update_idletasks()
 
-
-
                 percent = StringVar()
                 text = StringVar()
                 bar = Progressbar(root, orient=HORIZONTAL, length=220)
                 bar.place(x= 450, y=340)
                 Label(self.root, textvariable=percent, bg="white").place(x=700, y=340)
-                #Label(self.root, textvariable=text, bg="white").place(x=500, y=370)
                 start()
                 messagebox.showinfo(title=None, message="files uploaded successfully")
                 main_page()
@@ -70,24 +66,16 @@ class Login:
                 print(root.filename)
                 #shutil.copyfile(root.filename, rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}\{directory_whoto}" )
 
-
-
-
-
-
-            Label(text="Both", font=("Impact", 35, "bold"), fg="#6162FF", bg="white").place(x=500, y=180)
+            Label(text="Even & IDF", font=("Impact", 35, "bold"), fg="black", bg="white").place(x=470, y=180)
             Button(cursor="hand2", text="Return", bd=0, command=main_page, font=("Goudy old style", 15),
                    bg="#6162FF", fg="white").place(x=600, y=475, width=100, height=40)
             Button(cursor="hand2", text="Upload files", bd=0, command=files, font=("Goudy old style", 15),
-                   bg="#6162FF", fg="white").place(x=600, y=400, width=100, height=40)
+                fg="black").place(x=470, y=300, width=200, height=100)
             Button(cursor="hand2", text="Confirm", bd=0, command=prog, font=("Goudy old style", 15),
                    bg="#6162FF", fg="white").place(x=450, y=475, width=100, height=40)
-            Label(text="Drag Files here", font=("Goudy old style", 15, "bold"), fg="black", bg="white").place(
-                x=480, y=250)
 
         def even_t_page():
-            Register = Frame(self.root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
+            Frame(self.root, bg="white").place(x=330, y=150, width=500, height=400)
             Label(text="Both", font=("Impact", 35, "bold"), fg="#6162FF", bg="white").place(x=500, y=180)
             Button(cursor="hand2", text="Return", bd=0, command=main_page, font=("Goudy old style", 15),
                    bg="#6162FF", fg="white").place(x=600, y=475, width=100, height=40)
@@ -97,8 +85,7 @@ class Login:
                 x=480, y=250)
 
         def idf_t_page():
-            Register = Frame(self.root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
+            Frame(self.root, bg="white").place(x=330, y=150, width=500, height=400)
             Label(text="Both", font=("Impact", 35, "bold"), fg="#6162FF", bg="white").place(x=500, y=180)
             Button(cursor="hand2", text="Return", bd=0, command=main_page, font=("Goudy old style", 15),
                    bg="#6162FF", fg="white").place(x=600, y=475, width=100, height=40)
@@ -108,21 +95,18 @@ class Login:
                 x=480, y=250)
 
         def to_both():
-            Register = Frame(self.root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
+            Frame(self.root, bg="white", ).place(x=330, y=150, width=500, height=400)
             self.whoto = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.whoto.place(x=470, y=390)
             self.name = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.name.place(x=470, y=310)
 
             def create_folder():
-                parent_dir = rf"\\mnhazfs\nonword\halbanot\Both"
                 directory_name = self.name.get()
                 final = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}")
                 if not final:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}")
                 directory_whoto = self.whoto.get()
-                parent_dir2 = rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}"
                 final2 = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}\{directory_whoto}")
                 if not final2:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\Both\{directory_name}\{directory_whoto}")
@@ -141,21 +125,18 @@ class Login:
                 x=520, y=350)
 
         def to_even():
-            Register = Frame(self.root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
+            Frame(self.root, bg="white", ).place(x=330, y=150, width=500, height=400)
             self.whoto = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.whoto.place(x=470, y=390)
             self.name = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.name.place(x=470, y=310)
 
             def create_folder():
-                parent_dir = rf"\\mnhazfs\nonword\halbanot\Even"
                 directory_name = self.name.get()
                 final = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\Even\{directory_name}")
                 if not final:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\Even\{directory_name}")
                 directory_whoto = self.whoto.get()
-                parent_dir2 = rf"\\mnhazfs\nonword\halbanot\Even\{directory_name}"
                 final2 = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\Even\{directory_name}\{directory_whoto}")
                 if not final2:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\Even\{directory_name}\{directory_whoto}")
@@ -172,21 +153,18 @@ class Login:
                 x=530, y=350)
 
         def to_idf():
-            Register = Frame(self.root, bg="white", )
-            Register.place(x=330, y=150, width=500, height=400)
+            Frame(self.root, bg="white", ).place(x=330, y=150, width=500, height=400)
             self.whoto = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.whoto.place(x=470, y=390)
             self.name = Entry(font=("Goudy old style", 15), bg="#E7E6E6")
             self.name.place(x=470, y=310)
 
             def create_folder():
-                parent_dir = rf"\\mnhazfs\nonword\halbanot\IDF"
                 directory_name = self.name.get()
                 final = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\IDF\{directory_name}")
                 if not final:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\IDF\{directory_name}")
                 directory_whoto = self.whoto.get()
-                parent_dir2 = rf"\\mnhazfs\nonword\halbanot\IDF\{directory_name}"
                 final2 = os.path.isdir(rf"\\mnhazfs\nonword\halbanot\IDF\{directory_name}\{directory_whoto}")
                 if not final2:
                     os.mkdir(rf"\\mnhazfs\nonword\halbanot\IDF\{directory_name}\{directory_whoto}")
@@ -208,11 +186,8 @@ class Login:
 
 root = Tk()
 
-# Open Image
 bg_pic = Image.open("idk.jpg")
-# Resized Image
 resized = bg_pic.resize((1199, 850), Image.ANTIALIAS)
-
 bg = ImageTk.PhotoImage(resized)
 label_bgImage = Label(root, image=bg)
 label_bgImage.place(x=0, y=0)
